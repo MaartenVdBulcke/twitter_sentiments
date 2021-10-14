74487df719ac1e78da6cfe419d0a2985787ef92c
Maarten Van den Bulcke <maarten.vdbul@gmail.com>
2021-10-14 16:07:46 +0200

    reqs

--- utils/scrape.py
+++ utils/scrape.py
@@ -17,10 +17,10 @@ def scrape_twitter_for_hashtag(hashtag):
     st.write('before search')
     twint.run.Search(c)
     st.write('after search')
-    # tweet_list = c.search_tweet_list
-    scraped_tweets = twint.storage.panda.Tweets_df
-    scraped_tweets = scraped_tweets[scraped_tweets.language == 'en']
-    scraped_tweets.reset_index(drop=True, inplace=True)
-    # scraped_tweets = pd.DataFrame({'tweet':tweet_list})
+    tweet_list = c.search_tweet_list
+    # scraped_tweets = twint.storage.panda.Tweets_df
+    # scraped_tweets = scraped_tweets[scraped_tweets.language == 'en']
+    # scraped_tweets.reset_index(drop=True, inplace=True)
+    scraped_tweets = pd.DataFrame({'tweet':tweet_list})
     st.write('after reset_index')
     return scraped_tweets
