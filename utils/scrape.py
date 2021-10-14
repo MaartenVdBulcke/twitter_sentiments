5daf1c2c857f2b8ea776c119adc96139a3304b4e
Maarten Van den Bulcke <maarten.vdbul@gmail.com>
2021-10-14 19:43:23 +0200

    back to my function and more tweets: 5000

--- utils/scrape.py
+++ utils/scrape.py
@@ -25,23 +25,10 @@ def scrape_twitter_for_hashtag(hashtag):
     c = twint.Config()
     c.Search = hashtag
     c.Lang = 'en'
-    c.Limit = 5000
+    c.Limit = 10000
     c.Pandas = True
-    # c.Store_csv = True
-    # c.Output = 'tweets.csv'
-    # c.Hide_output = True
-    # nest_asyncio.apply()
-    # loop = asyncio.new_event_loop()
-    # asyncio.set_event_loop(loop)
-    # asyncio.set_event_loop(asyncio.new_event_loop())
-    # st.write('before search')
-    # twint.run.Search(c)
-    # st.write('after search')
-    # tweet_list = c.search_tweet_list
     scraped_tweets = twint.storage.panda.Tweets_df
-    # st.write(scraped_tweets)
     scraped_tweets = scraped_tweets[scraped_tweets.language == 'en']
     scraped_tweets.reset_index(drop=True, inplace=True)
-    # scraped_tweets = pd.DataFrame({'tweet':tweet_list})
     st.write('after reset_index')
     return scraped_tweets
