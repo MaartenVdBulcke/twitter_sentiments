70c0442f3c718f0c5749a2d017d27b012785e5dd
Maarten Van den Bulcke <maarten.vdbul@gmail.com>
2021-10-14 13:09:32 +0200

    reset for docker

--- utils/scrape.py
+++ utils/scrape.py
@@ -7,12 +7,12 @@ def scrape_twitter_for_hashtag(hashtag):
     c.Lang = 'en'
     c.Limit = 50
     c.Search = hashtag
-    # c.Pandas = True
-    # c.Hide_output = True
+    c.Pandas = True
+    c.Hide_output = True
     twint.run.Search(c)
-    tweet_list = c.search_tweet_list
-    # scraped_tweets = twint.storage.panda.Tweets_df
-    # scraped_tweets = scraped_tweets[scraped_tweets.language == 'en']
-    # scraped_tweets.reset_index(drop=True, inplace=True)
-    scraped_tweets = pd.DataFrame({'tweet':tweet_list})
+    # tweet_list = c.search_tweet_list
+    scraped_tweets = twint.storage.panda.Tweets_df
+    scraped_tweets = scraped_tweets[scraped_tweets.language == 'en']
+    scraped_tweets.reset_index(drop=True, inplace=True)
+    # scraped_tweets = pd.DataFrame({'tweet':tweet_list})
     return scraped_tweets
