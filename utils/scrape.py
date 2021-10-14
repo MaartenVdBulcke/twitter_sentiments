1b8e16361d0c42a9cb8131b81f614d3e6348d963
Maarten Van den Bulcke <maarten.vdbul@gmail.com>
2021-10-14 16:26:04 +0200

    reqs

--- utils/scrape.py
+++ utils/scrape.py
@@ -1,8 +1,28 @@
 import streamlit as st
 import pandas as pd
 import twint
+import os
 # import nest_asyncio
-# import asyncio
+import asyncio
+
+
+def load_tweet(hashtag):
+  result_loc = 'tweets.csv'
+  c = twint.Config()
+  c.Search = hashtag
+  c.Limit = 50
+  c.Lang = 'en'
+  c.Custom["tweet"] = ['date','tweet','username']
+  c.Output = result_loc
+  c.Store_csv = True
+  asyncio.set_event_loop(asyncio.new_event_loop())
+  twint.run.Search(c)
+  st.write('after search')
+  df = pd.read_csv(result_loc)
+  os.remove(result_loc)
+  st.write('tweets.csv')
+  return df
+
 
 def scrape_twitter_for_hashtag(hashtag):
     c = twint.Config()
