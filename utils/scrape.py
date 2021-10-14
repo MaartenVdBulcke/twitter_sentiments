1d5483b3464ee4c1e8ccf115fbeba85eeb4eecdd
Maarten Van den Bulcke <maarten.vdbul@gmail.com>
2021-10-14 15:56:47 +0200

    reqs

--- utils/scrape.py
+++ utils/scrape.py
@@ -13,10 +13,13 @@ def scrape_twitter_for_hashtag(hashtag):
     # nest_asyncio.apply()
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
+    print('before search')
     twint.run.Search(c)
+    print('after search')
     # tweet_list = c.search_tweet_list
     scraped_tweets = twint.storage.panda.Tweets_df
     scraped_tweets = scraped_tweets[scraped_tweets.language == 'en']
     scraped_tweets.reset_index(drop=True, inplace=True)
     # scraped_tweets = pd.DataFrame({'tweet':tweet_list})
+    print('after reset_index')
     return scraped_tweets
